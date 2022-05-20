1e58b4b745faba89dc8a596555492752f065526f
tgolais <skpassegna@gmail.com>
2022-05-20 22:01:55 +0000

    Start

---
+++ README.md
@@ -0,0 +1,2 @@
+#Instachat
+Instachat is junk chat system built in php for the event [PHP daily on Facebook](https://www.facebook.com/events/1151987242318474/)
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+	
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-6 mt-5 mx-auto">
+				<h1>Welcome</h1>
+				
+				<p>
+					<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum ipsum, corrupti nemo quam, at officiis repellat quos veritatis aspernatur delectus et, mollitia nulla magnam explicabo, consectetur obcaecati a saepe perferendis.</span>
+					<span>Aperiam quasi temporibus, explicabo quod sapiente, ab nisi placeat officiis. Officia, quo? Dolore quisquam animi ullam voluptatum vitae error sed nemo, inventore omnis nisi consequatur pariatur a sapiente aliquam, iste.</span>
+					<span>Necessitatibus dolor voluptatum nobis consequatur dolorum in rem doloremque velit! Architecto accusamus ea nisi laborum! Animi, rerum, nisi corporis nam quas maxime cupiditate quasi error veritatis eaque eveniet, sit vero.</span>
+				</p>
+				<a href="#" class="btn btn-outline-dark btn-lg">Open/Start session</a>
+			</div>
+		</div>
+		
+		
+	</div>
+	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
